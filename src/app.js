1fb30574445b74807f91b90e5fc1cee1fe01f18f
Kate Deras <65469487+quackate@users.noreply.github.com>
2023-07-05 14:24:43 +0000

    First commit, project finished

--- src/app.js
+++ src/app.js
@@ -6,6 +6,26 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector(".card").classList.add(switchIcons());
+
+  document.querySelector("#click").addEventListener("click", () => {
+    document
+      .querySelector(".card")
+      .classList.remove("diamond", "heart", "spade", "club");
+    document.querySelector(".card").classList.add(switchIcons());
+    document.querySelector(".card-number").innerHTML = switchNumber();
+  });
+};
+
+//My code is right below here!
+const switchNumber = () => {
+  let numbers = ["A", 2, 3, 4, 5, 6, 7, 8, 9, "J", "Q", "K"];
+  let numIndex = Math.floor(Math.random() * numbers.length);
+  return numbers[numIndex];
+};
+
+const switchIcons = () => {
+  let icons = ["diamond", "heart", "spade", "club"];
+  let iconsIndex = Math.floor(Math.random() * icons.length);
+  return icons[iconsIndex];
 };
